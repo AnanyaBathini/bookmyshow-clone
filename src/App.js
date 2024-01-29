60608d56d06e3488b1412a80ed7ca3b7c78291c4
ananyabhathini <ananyabhathini37@gmail.com>
2024-01-29 21:42:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App p-10">
+      <h1 className='text-Ananya-500'>Ananya Bathini</h1>
+     {/* <h1 className='text-6xl hover:text-blue-500'>Hey react application</h1> */}
+     <button className='border-2 px-2 py-1 border-blue-300 md:border-red-300 lg:border-green-300 rounded bg-blue-500 md:bg-red-500 lg:bg-green-500 text-white hover:bg-transparent hover:text-blue-500 md:hover:text-red-500 lg:hover:text-green-500 hover:border-4'>Hello</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        Ananya:
+        
+{
+  50: '#fff8dc',
+  100: '#ffeeaf',
+  200: '#fde77f',
+  300: '#fde34f',
+  400: '#fccc20',
+  500: '#e3a50a',
+  600: '#b07402',
+  700: '#7e4a00',
+  800: '#4c2800',
+  900: '#1a0c00',
+}
+      }
+    },
+  },
+  plugins: [],
+}
